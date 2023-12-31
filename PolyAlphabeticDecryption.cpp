17f44b3445c5212870e3e1235f8cd4f5713e3e58
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-12-31 10:56:08 +0600

    Update code of PolyAlphabetic Cipher

--- PolyAlphabeticDecryption.cpp
+++ PolyAlphabeticDecryption.cpp
@@ -2,39 +2,45 @@
 
 using namespace std;
 
-string decryptionVigenere(string cipherText, string keyword, int len);
+string decryptionVigenere(string cipherText, string keyword);
 
 
 int main(){
-    string cipherText, keyword;
+    string cipherText, keyword, newKeyword, plainText;
     cout << "Enter Cipher Text(capital letter): ";
     getline(cin, cipherText);
     cout << "Enter keyword: ";
     getline(cin,keyword);
     int len = keyword.size();
-    string plainText = decryptionVigenere(cipherText, keyword, len);
+    if(len < cipherText.size()){
+        newKeyword = keyword;
+        int i=0;
+        while(len < cipherText.size()){
+            newKeyword.push_back(keyword[i]);
+            i++;
+            len++;
+            if(i==keyword.size()){
+                i=0;
+            }
+        }
+        cout << newKeyword << endl;
+        plainText = decryptionVigenere(cipherText, newKeyword);
+    }else{
+        plainText = decryptionVigenere(cipherText, keyword);
+    }
     cout << "Plain Text: " << plainText << endl;
     return 0;
 }
 
-string decryptionVigenere(string cipherText, string keyword,int len){
+string decryptionVigenere(string input, string keyword){
     string plainText = "";
-    int sizeInput = cipherText.size();
-    int counter = 0;
-    int i = 0;
-    while(sizeInput){
-        int temp = (cipherText[i] - keyword[counter]);
+    for(int i=0; i<input.size(); i++){
+        int temp = ((input[i]- 'A') - (keyword[i] - 'A'));
         if(temp < 0){
-            temp += 26;
+            temp+=26;
         }
         char c = temp + 'A';
-        plainText += c;
-        counter++;
-        if(counter==len){
-            counter = 0;
-        }
-        i++;
-        sizeInput--;
+        plainText+=c;
     }
     return plainText;
 }
--- PolyAlphabeticEncryption.cpp
+++ PolyAlphabeticEncryption.cpp
@@ -2,40 +2,43 @@
 
 using namespace std;
 
-string encryptionVigenere(string plainText, int arr[], int len);
+string encryptionVigenere(string plainText, string keyword);
 
 int main(){
-    string plainText, keyword;
+    string plainText, keyword, newKeyword, cipherText;
     cout << "Enter Plain Text(capital letter): ";
     getline(cin, plainText);
     cout << "Enter keyword: ";
     getline(cin,keyword);
     int len = keyword.size();
-    int arr[len];
-    for(int i=0; i<len; i++){
-        arr[i] = keyword[i] - 'A';
+    if(len < plainText.size()){
+        newKeyword = keyword;
+        int i=0;
+        while(len < plainText.size()){
+            newKeyword.push_back(keyword[i]);
+            i++;
+            len++;
+            if(i==keyword.size()){
+                i=0;
+            }
+        }
+        cout << newKeyword << endl;
+        cipherText = encryptionVigenere(plainText, newKeyword);
     }
-    string cipherText = encryptionVigenere(plainText, arr, len);
+    else{
+        cipherText = encryptionVigenere(plainText, keyword);
+    }
 
     cout << "Cipher Text: " << cipherText << endl;
     return 0;
 }
 
-string encryptionVigenere(string input, int arr[], int len){
+string encryptionVigenere(string input, string keyword){
     string cipherText = "";
-    int sizeInput = input.size();
-    int counter = 0;
-    int i = 0;
-    while(sizeInput){
-        int temp = (input[i] - 'A' + arr[i]) % 26;
+    for(int i=0; i<input.size(); i++){
+        int temp = ((input[i]-'A') + (keyword[i] - 'A')) % 26;
         char c = temp + 'A';
         cipherText+=c;
-        counter++;
-        if(counter==len){
-            counter=0;
-        }
-        i++;
-        sizeInput--;
     }
     return cipherText;
 }
